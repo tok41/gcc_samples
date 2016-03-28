52d2b2cb2e40cb8cb2b59d4a7ebe154a9b9d10af
tok41 <hippy.hikky@gmail.com>
2016-03-28 11:05:41 +0900

    mutexなどをmap化

--- thread_sample/src/cond_var_sample_3.cpp
+++ thread_sample/src/cond_var_sample_3.cpp
@@ -5,12 +5,11 @@
 #include <thread>
 #include <string.h>
 #include <unistd.h>
+#include <map>
 
-std::mutex mtx1;
-std::mutex mtx2;
+std::map<int, std::mutex*> mtx_map;
 
-std::condition_variable cond;
-std::condition_variable cond2;
+std::map<int, std::condition_variable*> cond_map;
 
 bool retrieved1 = false;
 bool retrieved2 = false;
@@ -20,38 +19,42 @@ bool f2 = true;
 
 void do_worker1 (int idx) {
   while( f1 ) {
-	std::unique_lock<std::mutex> lock( mtx1 ); // mutex獲る
-	cond.wait(lock); // 待つ
+	std::unique_lock<std::mutex> lock( *(mtx_map[1]) ); // mutex獲る
+	cond_map[1]->wait(lock); // 待つ
 	
 	for(int i=0 ; i<3 ; i++){
 	  std::cout << idx << " : " << i << std::endl;
 	  sleep(1);
 	}
-	cond.notify_all(); // 作業終わったので通知
+	cond_map[1]->notify_all(); // 作業終わったので通知
   }
 }
 
 void do_worker2 (int idx) {
   while( f2 ) {
-	std::unique_lock<std::mutex> lock( mtx2 );
-	cond2.wait(lock); // 待つ
+	std::unique_lock<std::mutex> lock( *(mtx_map[2]) );
+	cond_map[2]->wait(lock); // 待つ
 	
 	for(int i=0 ; i<3 ; i++){
 	  std::cout << idx << " : " << i << std::endl;
 	  sleep(1);
 	}
-	cond2.notify_all(); // 作業終わったので通知
+	cond_map[2]->notify_all(); // 作業終わったので通知
   }
 }
 
 int main(int argc, char const* argv[]){
+  mtx_map[1] = new std::mutex;
+  mtx_map[2] = new std::mutex;
+  cond_map[1] = new std::condition_variable;
+  cond_map[2] = new std::condition_variable;
   std::thread t1(do_worker1, 1);
   std::thread t2(do_worker2, 2);
 
   std::string s;
   std::cin >> s;
   std::cout << "入力文字 : " << s << std::endl;
-  cond.notify_all();
+  cond_map[1]->notify_all();
   while (true ) {
 	std::cin >> s;
 	std::cout << "入力文字 : " << s << std::endl;
@@ -60,13 +63,13 @@ int main(int argc, char const* argv[]){
 	if(s=="q") {
 	  f1 = false;
 	  f2 = false;
-	  cond.notify_all();
-	  cond2.notify_all();
+	  cond_map[1]->notify_all();
+	  cond_map[2]->notify_all();
 	  break;
 	} else if(s=="1") {
-	  cond.notify_all();
+	  cond_map[1]->notify_all();
 	} else {
-	  cond2.notify_all();
+	  cond_map[2]->notify_all();
 	}
 	
   }
